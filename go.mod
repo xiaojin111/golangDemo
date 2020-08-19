de91449202c4d74d9795542808beba0600f2c008
xiaojin <706901586@qq.com>
2020-08-19 16:19:21 +0800

    ginTest

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.14
 require (
 	github.com/TeaEntityLab/fpGo v1.1.3
 	github.com/garyburd/redigo v1.6.0 // indirect
+	github.com/go-redis/redis/v8 v8.0.0-beta.7
+	github.com/lesismal/arpc v0.0.0-20200709074026-62b69a79e929 // indirect
 	github.com/panjf2000/ants/v2 v2.4.1 // indirect
 )
--- redisTest/main.go
+++ redisTest/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"log"
+	"time"
+)
+
 func main() {
+	log.SetFlags(log.Lshortfile)
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	pong, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(pong)
+
+	rdb.Set(context.Background(), "nameq", "张三", 10*time.Second)
+	name, _ := rdb.Get(context.Background(), "nameq").Result()
+	log.Println(name)
 
 }
